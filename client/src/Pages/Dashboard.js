df100f4ee3a7d5cb1109ae7076ec051682f51e87
SACHIN SINGH <92503784+sachinsingh3232@users.noreply.github.com>
2024-02-09 16:53:30 +0530

    Update Dashboard.js

--- client/src/Pages/Dashboard.js
+++ client/src/Pages/Dashboard.js
@@ -6,20 +6,20 @@ import axios from 'axios'
 
 const Dashboard = () => {
     const [links, setLinks] = useState([]);
-    // useEffect(() => {
-    //     const fun = async () => {
-    //         try {
-    //             const resp = await axios.get(`${process.env.REACT_APP_API_URL}/url/getAllUserURL`,
-    //                 { headers: { "Content-Type": "application/json" }, withCredentials: true }
-    //             )
-    //             // console.log(resp.data.data);
-    //             setLinks(resp?.data?.data);
-    //         } catch (e) {
-    //             console.log(e)
-    //         }
-    //     }
-    //     fun();
-    // }, [])
+    useEffect(() => {
+        const fun = async () => {
+            try {
+                const resp = await axios.get(`${process.env.REACT_APP_API_URL}/url/getAllUserURL`,
+                    { headers: { "Content-Type": "application/json" }, withCredentials: true }
+                )
+                // console.log(resp.data.data);
+                setLinks(resp?.data?.data);
+            } catch () {
+                // console.log(e)
+            }
+        }
+        fun();
+    }, [])
 
     return (
         <div className='Dashboard'>
