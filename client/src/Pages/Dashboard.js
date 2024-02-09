cd43c9152ac8a6c8f7ab027d5cd2ef879e72bfe8
SACHIN SINGH <92503784+sachinsingh3232@users.noreply.github.com>
2024-02-09 16:48:11 +0530

    Update Dashboard.js

--- client/src/Pages/Dashboard.js
+++ client/src/Pages/Dashboard.js
@@ -6,29 +6,29 @@ import axios from 'axios'
 
 const Dashboard = () => {
     const [links, setLinks] = useState([]);
-    useEffect(() => {
-        const fun = async () => {
-            try {
-                const resp = await axios.get(`${process.env.REACT_APP_API_URL}/url/getAllUserURL`,
-                    { headers: { "Content-Type": "application/json" }, withCredentials: true }
-                )
-                // console.log(resp.data.data);
-                setLinks(resp?.data?.data);
-            } catch (e) {
-                console.log(e)
-            }
-        }
-        fun();
-    }, [])
+    // useEffect(() => {
+    //     const fun = async () => {
+    //         try {
+    //             const resp = await axios.get(`${process.env.REACT_APP_API_URL}/url/getAllUserURL`,
+    //                 { headers: { "Content-Type": "application/json" }, withCredentials: true }
+    //             )
+    //             // console.log(resp.data.data);
+    //             setLinks(resp?.data?.data);
+    //         } catch (e) {
+    //             console.log(e)
+    //         }
+    //     }
+    //     fun();
+    // }, [])
 
     return (
         <div className='Dashboard'>
             <Navbar />
-            <div className='cardContainer'>
-                {links.length > 0 && links.map((link, i) => (
-                    <Card key={i} link={link} />
-                ))}
-            </div>
+            // <div className='cardContainer'>
+            //     {links.length > 0 && links.map((link, i) => (
+            //         <Card key={i} link={link} />
+            //     ))}
+            // </div>
         </div>
     )
 }
