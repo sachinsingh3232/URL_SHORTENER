d598cc04cbbea1812ab2512e6a4b9810df2ef719
SACHIN SINGH <92503784+sachinsingh3232@users.noreply.github.com>
2024-02-09 17:35:31 +0530

    Update Dashboard.js

--- client/src/Pages/Dashboard.js
+++ client/src/Pages/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from "react-router-dom";
 import Card from '../components/Card'
 import './dashboard.css'
 import Navbar from '../components/Navbar'
@@ -6,6 +7,11 @@ import axios from 'axios'
 
 const Dashboard = () => {
     const [links, setLinks] = useState([]);
+    const navigate = useNavigate();
+    useEffect(() => {
+        const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+        if (!userInfo) navigate('/login');
+    }, [navigate])
     useEffect(() => {
         const fun = async () => {
             try {
